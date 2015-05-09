42c2c6f15874aa37e928a8f72cad0b6bbd865dc7
martin2250 <martin.225@web.de>
2015-05-09 14:15:35 +0200

    Added HeatMapColor.cs

---
+++ HeatMapColor.cs
@@ -0,0 +1,43 @@
+private Color ColorFromHeight(float val, float min, float max)
+{
+	val -= min;
+	val /= (max - min);
+
+	float r = 0, g = 0, b = 0;
+
+	if(val < 0.2)
+	{
+		b = 1;
+		g = 5 * val;
+	}
+	else if(val < 0.4)
+	{
+		val -= 0.2F;
+
+		g = 1;
+		b = 1 - 5 * val;
+	}
+	else if(val < 0.6)
+	{
+		val -= 0.4F;
+
+		g = 1;
+		r = 5 * val;
+	}
+	else if (val < 0.8)
+	{
+		val -= 0.6F;
+
+		r = 1;
+		g = 1 - 5 * val;
+	}
+	else
+	{
+		val -= 0.8F;
+
+		r = 1;
+		b = 5 * val;
+	}
+
+	return Color.FromArgb(255, (int)(r * 255), (int)(g * 255), (int)(b * 255));
+}
