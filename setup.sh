bfd9d7d7b6b97f4603bf2b8526219f7c8883100f
Martin Pittermann <martinp2250@gmail.com>
2017-03-09 11:33:12 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,6 @@ sudo apt-get install -y picoscope
 sudo apt-get install python-pip python-dev
 pip install --upgrade pip
 sudo pip install setuptools
-sudo pip install matplotlib numpy
+sudo pip install matplotlib numpy scipy
 
 sudo apt-get install texlive texlive-fonts-recommended texlive-lang-german texlive-latex-base texlive-latex-extra texlive-latex-recommended lmodern texlive-fonts-recommended
