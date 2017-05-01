df7aeb617f7f2075bc9a7ebabbcd8c061f405525
Martin Pittermann <martinp2250@gmail.com>
2017-05-01 19:59:23 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,6 +9,7 @@ sudo pip install matplotlib numpy scipy
 
 sudo apt-get install texlive texlive-fonts-recommended texlive-lang-german texlive-latex-base texlive-latex-extra texlive-latex-recommended lmodern texlive-fonts-recommended
 
-echo "alias dd='dd status=progress'" >> .bashrc
-echo "alias fuck='sudo \$(history -p \!\!)'" >> .bashrc
-echo "alias sudo='sudo '" >> .bashrc    #make above aliases work with sudo
+echo "alias dd='dd status=progress'" >> ~/.bashrc
+echo "alias fuck='sudo \$(history -p \!\!)'" >> ~/.bashrc
+echo "alias sudo='sudo '" >> ~/.bashrc    #make above aliases work with sudo
+echo "function transfer { curl --upload-file $1 https://transfer.sh/$1; echo; }" > ~/.bashrc
