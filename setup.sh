cf9dcee0e6339ed729433c00fdae3bd0b527e33d
Martin Pittermann <martinp2250@gmail.com>
2017-05-01 21:46:27 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,24 @@
-sudo bash -c 'echo "deb http://labs.picotech.com/debian/ picoscope main" >/etc/apt/sources.list.d/picoscope.list'
-sudo apt-get update
-sudo apt-get install -y picoscope
-
+#python environment
 sudo apt-get install python-pip python-dev
 pip install --upgrade pip
 sudo pip install setuptools
 sudo pip install matplotlib numpy scipy
 
+#latex compiler
 sudo apt-get install texlive texlive-fonts-recommended texlive-lang-german texlive-latex-base texlive-latex-extra texlive-latex-recommended lmodern texlive-fonts-recommended
 
+#fuck
+read -p "Use thefuck? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	pip install --user thefuck
+	echo "eval $(thefuck --alias)" >> ~/.bashrc
+else
+	echo "alias fuck='sudo \$(history -p \!\!)'" >> ~/.bashrc
+fi
+
+#aliases
 echo "alias dd='dd status=progress'" >> ~/.bashrc
-echo "alias fuck='sudo \$(history -p \!\!)'" >> ~/.bashrc
-echo "alias sudo='sudo '" >> ~/.bashrc    #make above aliases work with sudo
+echo "alias sudo='sudo '" >> ~/.bashrc    #make aliases work with sudo
 echo "function transfer { curl --upload-file $1 https://transfer.sh/$1; echo; }" > ~/.bashrc
